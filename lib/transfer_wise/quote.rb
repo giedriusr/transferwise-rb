4479b6119edd4da7ba2c38ac907fcda90815015f
Giedrius Rimkus <info@giedriusr.lt>
2017-07-20 09:26:36 +0300

    Add account requirements endpoint

--- lib/transfer_wise/quote.rb
+++ lib/transfer_wise/quote.rb
@@ -1,4 +1,12 @@
 module TransferWise
   class Quote < APIResource
+    def self.account_requirements(resource_id, headers = {})
+      response = TransferWise::Request.request(:get, account_requirements_url(resource_id), {}, headers)
+      convert_to_transfer_wise_object(response)
+    end
+
+    def self.account_requirements_url(resource_id)
+      "#{collection_url}/#{resource_id}/account-requirements"
+    end
   end
 end
